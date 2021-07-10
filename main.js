39a51414493549b95d1ea1bbf5371c025c5e1364
gabriel201o <80070076+gabriel201o@users.noreply.github.com>
2021-07-10 11:54:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var SpeechRecognition =window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("text_box").innerHTML = "";
+    recognition.start();
+}
+
+
+recognition.onresult = function(event){
+    console.log(event);
+
+    var Content=event.results[0][0].transcript;
+
+    document.getElementById("text_box").innerHTML = Content;
+    console.log(Content);
+    if(Content=="take my selfie"){
+        console.log("taking selfie in 5 seconds");
+        speak();
+    }
+    
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data ="taking selfie in 5 seconds";
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+      take_snapshot();
+      save();
+    }5000);
+}
+
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:"jpeg",
+    jpeg_quality:90,
+});
